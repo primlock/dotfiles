8aeb254d0ca418683c9c2bc6fbe3e986c9ae0e74
Alex Reynolds <areynolds0@proton.me>
2026-01-15 19:53:34 -0500

    stop bold text in the tabs

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,19 +1,36 @@
--- Bufferline is a plugin that provides a visual tab-like user interface, often at the top of the screen, to 
+-- Bufferline is a plugin that provides a visual tab-like user interface, often at the top of the screen, to
 -- manage and switch between open buffers
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
 
   config = function()
-    require('bufferline').setup({
+    require("bufferline").setup({
       options = {
-        buffer_close_icon = '󰅖',
-        modified_icon = '● ',
-        close_icon = ' ',
-        left_trunc_marker = ' ',
-        right_trunc_marker = ' ',
+        buffer_close_icon = "󰅖",
+        modified_icon = "● ",
+        close_icon = " ",
+        left_trunc_marker = " ",
+        right_trunc_marker = " ",
         separator_style = "slant",
-      }
+      },
+      highlights = {
+        -- Override the highlight group for the selected buffer
+        buffer_selected = {
+          bold = false, -- Set bold to false to disable
+          italic = false,
+        },
+        -- Also override for inactive buffers if they appear bold
+        buffer_inactive = {
+          bold = false,
+          italic = false,
+        },
+        -- Override the highlight group for the selected buffer's visible area
+        buffer_visible_selected = {
+          bold = false,
+          italic = false,
+        },
+      },
     })
 
     -- Cycle buffers
@@ -26,6 +43,5 @@ return {
 
     -- Close buffer
     vim.keymap.set("n", "<S-w>", "<cmd>bdelete<CR>", { silent = true })
-  end
+  end,
 }
-
